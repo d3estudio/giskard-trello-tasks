d2329346edc0bb548480bc24f407d58aa8048fac
Victor Gama <hey@vito.io>
2016-06-19 16:06:07 -0300

    Update code structure, extract remote calls to another file

---
+++ src/api-utils.js
@@ -0,0 +1,93 @@
+var Trello = require("node-trello");
+
+module.exports = {
+    /**
+     * Returns a new Trello API Client instance
+     * @return {Trello} Trello Client instance
+     */
+    getApiClient: function() {
+        return new Trello(process.env.trellotasks_key, process.env.trellotasks_token);
+    },
+
+    /**
+     * Gets the current list from the main board
+     * @param  {String} boardId ID of the board to have the list id extracted from
+     * @return {Promise}         Promise that will be resolved when the list is
+     *                           acquired
+     */
+    getListFromBoard: function(boardId) {
+        return new Promise((resolve, reject) => {
+            this.getApiClient().get(`/1/boards/${boardId}/lists`, (err, data) => {
+                if (err || !data || data.length < 1) {
+                    return reject('Não consegui achar a lista de tarefas :disappointed:');
+                }
+                var boardList = data.sort(a => new Date(a.name.split(" - ")));
+                resolve(boardList[0].id);
+            });
+        });
+    },
+
+    /**
+     * Creates a new card for the given user, in the given list, with the given
+     * title
+     * @param  {String} userId ID of the User to whom the card will be created
+     * @param  {String} listId ID of the list where the card will be added to
+     * @param  {String} title  Title of the card to be created
+     * @return {Promise}        Promise that will be resolved or rejected
+     *                          depending on the success of the operation
+     */
+    createCardForUser: function(userId, listId, title) {
+        return new Promise((resolve, reject) => {
+            var newCard = {
+                name: title,
+                idList: listId,
+                idMembers: userId
+            };
+            this.getApiClient().post("1/cards", newCard, (err, data) => {
+                if (err) {
+                    return reject('Não consegui criar seu card :disappointed:');
+                }
+                resolve(data.id);
+            });
+        });
+    },
+
+    /**
+     * Gets an ID for a user with a given handle
+     * @param  {String} handle Handle of the user to have its ID acquired
+     * @return {Promise}        Promise that will be resolved with the user's
+     *                          id, if found, or rejected.
+     */
+    getUserIdForHandle: function(handle) {
+        return new Promise((resolve, reject) => {
+            this.getApiClient().get("/1/search/members", {
+                query: handle
+            }, (err, data) => {
+                if (err) {
+                    return reject(`Não consegui achar seu usuário! Tem certeza que \`${handle}\` está certo? :disappointed:`);
+                }
+                resolve(data[0].id);
+            });
+        });
+    },
+
+    /**
+     * Creates a new checklist item with the given data
+     * @param  {AnyObject} data   Object describing the new checklist item
+     * @param  {logger} logger Logger instance, bound with module data, used
+     *                         for debugging purposes
+     * @return {Promise}        Promise that will be resolved or rejected
+     *                          depending on the operation result
+     */
+    createChecklistItemWithData: function(data, logger) {
+        return new Promise((resolve, reject) => {
+            this.getApiClient().post("/1/checklists", data, (err, data) => {
+                if (err) {
+                    logger.error(err);
+                    return reject();
+                }
+                resolve();
+            });
+        });
+    }
+}
--- src/trellocard.js
+++ src/trellocard.js
@@ -1,133 +1,72 @@
 var Base = requireBaseModule(),
-    Trello = require("node-trello");
+    ApiUtils = require('./api-utils'),
+    weekDays = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta'];
 
-var t = new Trello(process.env.trellotasks_key, process.env.trellotasks_token),
-    checkModel = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"],
-    username;
-
-//----------------------
-//Helper functions
-//---------------------
-
-function getRightList(boardId) {
-    return new Promise((resolve, reject) => {
-        t.get(`/1/boards/${boardId}/lists`, (err, data) => {
-            if (err) {
-                reject('Não consegui achar a lista de tarefas :disappointed:');
-            }
-            var boardList = data.sort((a) => {
-                return new Date(a.name.split(" - "));
-            });
-            resolve(boardList[0].id);
-        });
-    });
-}
-
-function getUserID(username) {
-    return new Promise((resolve, reject) => {
-        t.get("/1/search/members", {
-            query: username
-        }, (err, data) => {
-            if (err) {
-                reject('Não consegui achar seu usuário :disappointed:');
-            }
-            resolve(data[0].id);
-        });
-    });
-}
-
-
-function createCard(idList, username) {
-    return new Promise((resolve, reject) => {
-        getUserID(username)
-            .then((userID) => {
-                var newCard = {
-                    name: username,
-                    idList: idList,
-                    idMembers: userID
-                }
-                t.post("1/cards", newCard, (err, data) => {
-                    if (err) {
-                        reject('Não consegui criar seu card :disappointed:');
-                    }
-                    resolve(data.id);
-                });
-            });
-    });
-}
-
-function commitChecklistItem(newChecklist) {
-    return new Promise((resolve, reject) => {
-        t.post("/1/checklists", newChecklist, (err, data) => {
-            if (err) {
-                console.log(err);
-                return reject('Não consegui criar os checklists :disappointed:');
-            }
-            resolve();
-        });
-    });
-}
-
-function createChecklist(idCard) {
-    var i = 0;
-    console.log(idCard);
-    return Promise.all(checkModel.map((value) => commitChecklistItem({
-        idCard: idCard,
-        name: value,
-        pos: i++
-    })));
-}
-
-var Trellocard = function(bot) {
-    Base.call(this, bot);
+var TrelloCard = function() {
+    Base.call(this);
     this.respond(/(.*)?trello card(.*)?/i, (response) => {
-        username = response.user.name;
         response.sendTyping();
-        getRightList("SjRJiE2O")
-            .then((idList) => {
-                return response.user.getSocialNetworkHandle('trello')
-                    .then((handle) => {
-                        return Promise.resolve({
-                            handle: handle,
-                            list: idList
-                        });
-                    })
-                    .catch((err) => {
-                        return response.ask('Qual seu usário no Trello?', this.Context.REGEX, /(.*)/)
-                            .then((answer) => {
-                                return response.user.updateSocialNetworkHandle('trello', answer.match[1])
-                                    .then(() => {
-                                        return Promise.resolve({
-                                            handle: answer.match[1],
-                                            list: idList
-                                        });
-                                    })
-                                    .catch((err) => {
-                                        return Promise.reject('Não consegui memorizar seu usuário :disappointed:');
-                                    })
-                            })
-                            .catch((err) => {
-                                return Promise.reject('Preciso do seu usuário no trello! :disappointed:');
-                            });
+
+        response.user.getSocialNetworkHandle('trello')
+            .catch(() => this.requireUsername(response))
+            .then(handle => ApiUtils.getUserIdForHandle(handle))
+            .then(userId => {
+                return ApiUtils.getListFromBoard('SjRJiE2O')
+                    .then(listId => ({ userId, listId }));
+            })
+            .then(data => {
+                var cardTitle = response.user.name;
+                if(cardTitle.trim().length === 0) {
+                    cardTitle = response.user.username;
+                }
+                return ApiUtils.createCardForUser(data.userId, data.listId, cardTitle);
+            })
+            .then(cardId => {
+                var i = 0,
+                    promises = weekDays
+                        .map(v => ({ idCard: cardId, name: v, pos: i++ }))
+                        .map(d => ApiUtils.createChecklistItemWithData(d, this.logger));
+                return Promise.all(promises)
+                    .catch(ex => {
+                        return Promise.reject("Não consegui criar seu checklist. :disappointed:");
                     });
-                //return createCard(idList);
             })
-            .then((data) => {
-                return createCard(data.list, data.handle);
-            })
-            .then((idCard) => {
-                return createChecklist(idCard)
-                .catch((err) => {
-                    return Promise.reject(err);
-                });
-            })
-            .then(() => {
-                response.reply("Feito, amiguinho.");
-            })
-            .catch((err) => {
-                response.reply(err);
+            .then(() => response.reply('Feito, amiguinho.'))
+            .catch(err => {
+                if(err instanceof Error) {
+                    this.logger.error(err);
+                    return response.reply('Eita! Aconteceu algo bem esquisito aqui e eu não consegui fazer alguma coisa. Ou eu fiz. Mas não sei. Calma, tô confuso.');
+                }
+                response.reply(err)
             });
     });
 };
 
-module.exports = Base.setup(Trellocard);
+TrelloCard.prototype.requireUsername = function(response) {
+    return response.ask('Qual seu usuário no Trello?', this.Context.REGEX, /(.*)/)
+        .then((answer) => {
+            return response.user.updateSocialNetworkHandle('trello', answer.match[1])
+                .then(() => {
+                    response.reply('Ok, peraí...');
+                    response.sendTyping();
+                    return answer.match[1];
+                })
+                .catch((err) => {
+                    return Promise.reject({
+                        internal: true,
+                        message: 'Não consegui memorizar seu usuário :disappointed:'
+                    });
+                });
+        })
+        .catch((err) => {
+            if(err.internal) {
+                Promise.reject(err.message);
+            } else if(err instanceof Error && !err.internal) {
+                Promise.reject(err)
+            } else {
+                Promise.reject('Preciso do seu usuário no trello! :disappointed:');
+            }
+        });
+}
+
+module.exports = Base.setup(TrelloCard);
